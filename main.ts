5e3e1f9d71d8ffadf406769d9dd4dfa1e5716514
jimena-2009 <jimena.gonzalez@panam.ed.cr>
2025-12-04 09:59:59 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -30,22 +30,38 @@ scene.onOverlapTile(SpriteKind.Player, sprites.builtin.forestTiles23, function (
 })
 let mySprite: Sprite = null
 mySprite = sprites.create(img`
-    . . . . . . . e e e e . . . . . 
-    . . . . . e e 4 5 5 5 e e . . . 
-    . . . . e 4 5 6 2 2 7 6 6 e . . 
-    . . . e 5 6 6 7 2 2 6 4 4 4 e . 
-    . . e 5 2 2 7 6 6 4 5 5 5 5 4 . 
-    . e 5 6 2 2 8 8 5 5 5 5 5 4 5 4 
-    . e 5 6 7 7 8 5 4 5 4 5 5 5 5 4 
-    e 4 5 8 6 6 5 5 5 5 5 5 4 5 5 4 
-    e 5 c e 8 5 5 5 4 5 5 5 5 5 5 4 
-    e 5 c c e 5 4 5 5 5 4 5 5 5 e . 
-    e 5 c c 5 5 5 5 5 5 5 5 4 e . . 
-    e 5 e c 5 4 5 4 5 5 5 e e . . . 
-    e 5 e e 5 5 5 5 5 4 e . . . . . 
-    4 5 4 e 5 5 5 5 e e . . . . . . 
-    . 4 5 4 5 5 4 e . . . . . . . . 
-    . . 4 4 e e e . . . . . . . . . 
+    ...........ccccc66666...........
+    ........ccc4444444444666........
+    ......cc444444444bb4444466......
+    .....cb4444bb4444b5b444444b.....
+    ....eb4444b5b44444b44444444b....
+    ...ebb44444b4444444444b444446...
+    ..eb6bb444444444bb444b5b444446..
+    ..e6bb5b44444444b5b444b44bb44e..
+    .e66b4b4444444444b4444444b5b44e.
+    .e6bb444444444444444444444bb44e.
+    eb66b44444bb444444444444444444be
+    eb66bb444b5b44444444bb44444444be
+    fb666b444bb444444444b5b4444444bf
+    fcb666b44444444444444bb444444bcf
+    .fbb6666b44444444444444444444bf.
+    .efbb66666bb4444444444444444bfe.
+    .86fcbb66666bbb44444444444bcc688
+    8772effcbbbbbbbbbbbbbbbbcfc22778
+    87722222cccccccccccccccc22226678
+    f866622222222222222222222276686f
+    fef866677766667777776667777fffef
+    fbff877768f86777777666776fffffbf
+    fbeffeefffeff7766688effeeeefeb6f
+    f6bfffeffeeeeeeeeeeeeefeeeeebb6e
+    f66ddfffffeeeffeffeeeeeffeedb46e
+    .c66ddd4effffffeeeeeffff4ddb46e.
+    .fc6b4dddddddddddddddddddb444ee.
+    ..ff6bb444444444444444444444ee..
+    ....ffbbbb4444444444444444ee....
+    ......ffebbbbbb44444444eee......
+    .........fffffffcccccee.........
+    ................................
     `, SpriteKind.Player)
 controller.moveSprite(mySprite)
 tiles.setCurrentTilemap(tilemap`level1`)
